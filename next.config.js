fa0ad09d74736e5f06bf0792276e4064231d452c
Leon Zeng <ncuzengliang@163.com>
2024-05-23 23:55:10 +0800

    首次提交

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  latex: true,
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/;
+
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    fileLoaderRule.exclude = allowedSvgRegex;
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  redirects: () => [
+    {
+      source: "/docs/guide/:slug(typescript|latex|tailwind-css|mermaid)",
+      destination: "/docs/guide/advanced/:slug",
+      permanent: true,
+    },
+    {
+      source: "/docs/docs-theme/built-ins/:slug(callout|steps|tabs)",
+      destination: "/docs/guide/built-ins/:slug",
+      permanent: true,
+    },
+  ],
+});
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: (
+    <span>
+      <b>TransDocs</b>
+    </span>
+  ),
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Trans Docs",
   },
-}
+  feedback: { content: null },
+  editLink: {
+    component: null,
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    // const url =
+    //   "https://my-app.com" +
+    //   (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
-export default config
+    return (
+      <>
+        {/* <meta property="og:url" content={url} /> */}
+        <meta property="og:title" content={frontMatter.title || "TransDocs"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "TransDocs 最新最佳的翻译文档"}
+        />
+      </>
+    );
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    console.log(asPath);
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – TransDocs",
+      };
+    }
+  },
+};
+
+export default config;
